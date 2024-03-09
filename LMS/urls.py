7e9a8d8a2297026ae0e1f1856dc201923967344c
Yaju Shrestha <yajustha11@gmail.com>
2024-03-09 19:00:02 +0545

    path for add-book and categories

--- LMS/urls.py
+++ LMS/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import home, UserView
+from .views import home, UserView, categories, AddBook
 
 urlpatterns = [
-    
-    path('', home),
+    path('home/', home, name="home"),
+    path('add-book/',AddBook,name="add-book"),
     path('register/',UserView.as_view({'post':'register'}),name='register'),
-    path('login/',UserView.as_view({'post':'login'}),name='login')
+    path('login/',UserView.as_view({'post':'login'}),name='login'),
+    path('categories/',categories, name="categories"),
 ]
