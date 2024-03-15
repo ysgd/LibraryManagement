f6f692d8fa1ad8957c7407fc269a5e35de7b5c0a
Yaju Shrestha <yajustha11@gmail.com>
2024-03-15 21:31:38 +0545

    finalized urls

--- LMS/urls.py
+++ LMS/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('category/',CategoryViewSet.as_view({'get':'list','post':'create'})),
     path('category/<int:pk>/',CategoryViewSet.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
     path('author/',AuthorViewSet.as_view({'get':'list','post':'create'}),name="author"),
-    path('author/<int:pk>/',AuthorViewSet.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name="author-edit"),
+    path('author/<int:pk>/',AuthorViewSet.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
     path('add-book/',AddBook.as_view({'get':'list','post':'create'}),name="add-book"),
     path('add-book/<int:pk>/',AddBook.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
     path('register/',UserView.as_view({'post':'register'}),name='register'),
